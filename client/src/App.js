17651d8a43b805027c622172ced8d4495f6ab6c3
karan08ms <126221715+karan08ms@users.noreply.github.com>
2023-02-24 13:38:10 +0700

    commit .gitignore

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+         <div className="container">
+        <h1 className="text-center">Hello World!</h1>
+        <div className='text-center'><button className="btn btn-danger">Click me</button></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
